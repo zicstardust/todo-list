f2f12af369cedf711203c36fd67c82084b63c229
zic <git@zicstardust.com>
2023-12-15 22:13:22 -0400

    create test

--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import Todo
+from django.utils import timezone
 
-# Create your tests here.
+
+class TodoTestCase(TestCase):
+    def setUp(self):
+        Todo.objects.create(title="test", details="test")
+        Todo.objects.create(title="test2", details="test2")
+
+    def test_todo(self):
+        test = Todo.objects.get(title="test")
+        test2 = Todo.objects.get(title="test2")
+
+        self.assertEqual(test.title, 'test')
+        self.assertEqual(test.details, 'test')
+
+        self.assertEqual(test2.title, 'test2')
+        self.assertEqual(test2.details, 'test2')
